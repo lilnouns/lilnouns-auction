da63407c4a89ea28006d1d25102036d033c628fc
Milad Nekofar <milad@nekofar.com>
2024-08-30 04:06:11 +0400

    feat(pages): add locale-based redirection on home page
    implement locale-based redirection and load catalog for localization.

---
+++ src/pages/index.ts
@@ -0,0 +1,19 @@
+import { getLocale, loadCatalog } from '@/utils/locales'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace(`/${getLocale()}`)
+  }, [router])
+
+  return
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: { translation: await loadCatalog(getLocale()) },
+  }
+}
