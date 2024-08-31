4eae68c34c4f55af4319d04e125d357de2fc0c49
Milad Nekofar <milad@nekofar.com>
2024-08-31 20:42:15 +0400

    refactor(pages): improve SVG rendering and error handling
    Added `isLoading` state, updated function types, and streamlined SVG rendering to enhance user feedback and code maintainability.

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,10 +1,9 @@
-import { LocaleSwitcher, ThemeSwitcher } from '@/components'
 import { availableLocales, loadCatalog } from '@/utils'
 import { ImageData, getNounData } from '@lilnounsdao/assets'
 import { buildSVG } from '@lilnounsdao/sdk'
-import { Trans, t } from '@lingui/macro'
+import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
-import { GetStaticPropsContext } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
@@ -31,39 +30,47 @@ interface ApiResponse {
   seeds: SeedData[]
 }
 
-// SVGImage component to display the SVG
-function SVGImage({ svgBase64 }: { svgBase64: string }) {
-  return <img src={`data:image/svg+xml;base64,${svgBase64}`} alt="Noun SVG" />
+// Function to render the SVG
+const renderSVG = (seed: Seed) => {
+  const { parts, background } = getNounData(seed)
+  const svgBinary = buildSVG(parts, palette, background)
+  return btoa(svgBinary)
 }
 
-export default function Home() {
+// SVGImage component to display the SVG
+const SVGImage: React.FC<{ svgBase64: string }> = ({ svgBase64 }) => (
+  <img src={`data:image/svg+xml;base64,${svgBase64}`} alt="Noun SVG" />
+)
+
+const Home: React.FC = () => {
   const { i18n } = useLingui()
   const [seedsData, setSeedsData] = useState<SeedData[]>([])
-  const [error, setError] = useState<string | undefined>() // Use undefined instead of null
+  const [error, setError] = useState<string | undefined>()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const router = useRouter()
-  const { nounId } = router.query // Dynamically get the nounId from the URL
+  const { nounId } = router.query
 
   useEffect(() => {
-    // Ensure nounId is a single string
     if (!nounId || Array.isArray(nounId)) return
 
-    async function fetchData() {
+    const fetchData = async () => {
+      setIsLoading(true)
       try {
         const response = await fetch(`/api/seeds/${nounId}`)
         if (!response.ok) {
           throw new Error('Failed to fetch seed data')
         }
 
-        // Explicitly type the response data
         const data: ApiResponse = await response.json()
         setSeedsData(data.seeds)
       } catch (error_) {
-        // Explicitly handle the error type
         if (error_ instanceof Error) {
           setError(error_.message)
         } else {
           setError('An unexpected error occurred')
         }
+      } finally {
+        setIsLoading(false)
       }
     }
 
@@ -78,44 +85,24 @@ export default function Home() {
         </title>
       </Head>
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <section className="mt-16 py-8">
+        <section className="p-8">
           <div className="container">
-            <h1 className="mb-4 text-center text-4xl font-bold leading-tight text-gray-900 dark:text-white">
-              <Trans>Building a Multi-Lingual Website</Trans>
-            </h1>
-            <p className="mb-8 text-center text-lg text-gray-600 dark:text-gray-300">
-              <Trans>
-                Welcome to our Next.js and Lingui demo site! Discover the power
-                of combining Next.js, a powerful React framework for building
-                server-side rendered applications, with Lingui, the ultimate
-                solution for multi-lingual support in your web projects.
-              </Trans>
-            </p>
-            <div className="mb-4 flex justify-between text-center">
-              <ThemeSwitcher className="rounded-full border-2 border-gray-900 p-1 text-gray-900 dark:border-white dark:text-white" />
-              <LocaleSwitcher className="rounded-full border-2 border-gray-900 p-1 text-gray-900 dark:border-white dark:text-white" />
-            </div>
-            {/* Display Noun Seeds */}
             <div>
-              {error ? (
+              {isLoading ? (
+                <div>Loading...</div>
+              ) : error ? (
                 <div className="text-red-500">Error: {error}</div>
               ) : (
                 <div className="grid grid-cols-1 gap-4 text-gray-900 md:grid-cols-2 lg:grid-cols-6 dark:border-white dark:text-white">
-                  {seedsData.map(({ blockNumber, seed }) => {
-                    const { parts, background } = getNounData(seed)
-                    const svgBinary = buildSVG(parts, palette, background)
-                    const svgBase64 = btoa(svgBinary)
-
-                    return (
-                      <div
-                        key={blockNumber} // Use blockNumber as a unique key
-                        className="rounded border p-4 text-center shadow-sm"
-                      >
-                        <SVGImage svgBase64={svgBase64} />
-                        <div className="mt-2">Block Number: {blockNumber}</div>
-                      </div>
-                    )
-                  })}
+                  {seedsData.map(({ blockNumber, seed }) => (
+                    <div
+                      key={blockNumber}
+                      className="rounded border p-4 text-center shadow-sm"
+                    >
+                      <SVGImage svgBase64={renderSVG(seed)} />
+                      <div className="mt-2">Block Number: {blockNumber}</div>
+                    </div>
+                  ))}
                 </div>
               )}
             </div>
@@ -126,17 +113,25 @@ export default function Home() {
   )
 }
 
-export async function getStaticProps(
-  context: GetStaticPropsContext<{ locale: string }>,
-) {
+export const getStaticProps: GetStaticProps = async (context) => {
+  const locale = context.params?.locale as string | undefined
+
+  if (!locale) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
-    props: { translation: await loadCatalog(context.params!.locale!) },
+    props: { translation: await loadCatalog(locale) },
   }
 }
 
 export async function getStaticPaths() {
   return {
     paths: availableLocales.map((locale) => ({ params: { locale } })),
-    fallback: false,
+    fallback: false, // Revert to false to comply with next-on-pages restrictions
   }
 }
+
+export default Home
