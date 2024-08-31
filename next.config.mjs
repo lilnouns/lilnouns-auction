1c15302a602385ed0de8d62c1d87c88d1105ed45
Milad Nekofar <milad@nekofar.com>
2024-08-31 14:28:35 +0400

    chore(config): add `setupDevPlatform` for dev environment
    Ensure proper dev setup for Cloudflare integration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -31,4 +32,8 @@ const nextConfig = {
   },
 }
 
-export default nextConfig;
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
+}
+
+export default nextConfig
