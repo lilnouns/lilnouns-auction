7f072d2a9de079fcd0fe92dbc1bc84a9d74d29e0
Milad Nekofar <milad@nekofar.com>
2024-09-01 23:04:26 +0400

    refactor(app): move `WagmiProvider` and `QueryClientProvider`
    migrates `WagmiProvider` and `QueryClientProvider` to `_app.tsx` for better separation of concerns and to reduce redundancy.

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -5,22 +5,7 @@ import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import React, { useCallback, useEffect, useState } from 'react'
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider, createConfig, http } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-
-const queryClient = new QueryClient()
-
-const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-})
 
 const { palette } = ImageData
 
@@ -124,121 +109,119 @@ const Home: React.FC = () => {
   ])
 
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <Head>
-          <title>
-            {nounId
-              ? t(
-                  i18n,
-                )`Noun ${nounId} | Building a Multi-Lingual Website with Next.js & Lingui`
-              : t(i18n)`Building a Multi-Lingual Website with Next.js & Lingui`}
-          </title>
-          <meta
-            name="description"
-            content={
-              nounId
-                ? `Explore the details and seeds of Noun ${nounId} on our multi-lingual website built with Next.js and Lingui. Discover the power of decentralized creativity.`
-                : `Learn how to build a multi-lingual website using Next.js and Lingui. This demo site showcases the seamless integration of these powerful tools.`
-            }
-          />
-        </Head>
-        <div className="flex min-h-screen flex-col items-center justify-between bg-gray-50 p-24 dark:bg-gray-900">
-          <section className="p-8">
-            <div className="container">
-              <div className="mb-4">
-                <div className="grid grid-cols-2 gap-4">
-                  <select
-                    value={selectedBackground}
-                    onChange={(e) => setSelectedBackground(e.target.value)}
-                  >
-                    <option value="">Select Background</option>
-                    {ImageData.bgcolors.map((color, index) => (
-                      <option key={index} value={index.toString()}>
-                        {color}
-                      </option>
-                    ))}
-                  </select>
+    <>
+      <Head>
+        <title>
+          {nounId
+            ? t(
+                i18n,
+              )`Noun ${nounId} | Building a Multi-Lingual Website with Next.js & Lingui`
+            : t(i18n)`Building a Multi-Lingual Website with Next.js & Lingui`}
+        </title>
+        <meta
+          name="description"
+          content={
+            nounId
+              ? `Explore the details and seeds of Noun ${nounId} on our multi-lingual website built with Next.js and Lingui. Discover the power of decentralized creativity.`
+              : `Learn how to build a multi-lingual website using Next.js and Lingui. This demo site showcases the seamless integration of these powerful tools.`
+          }
+        />
+      </Head>
+      <div className="flex min-h-screen flex-col items-center justify-between bg-gray-50 p-24 dark:bg-gray-900">
+        <section className="p-8">
+          <div className="container">
+            <div className="mb-4">
+              <div className="grid grid-cols-2 gap-4">
+                <select
+                  value={selectedBackground}
+                  onChange={(e) => setSelectedBackground(e.target.value)}
+                >
+                  <option value="">Select Background</option>
+                  {ImageData.bgcolors.map((color, index) => (
+                    <option key={index} value={index.toString()}>
+                      {color}
+                    </option>
+                  ))}
+                </select>
 
-                  <select
-                    value={selectedBody}
-                    onChange={(e) => setSelectedBody(e.target.value)}
-                  >
-                    <option value="">Select Body</option>
-                    {ImageData.images.bodies.map((body, index) => (
-                      <option key={index} value={index.toString()}>
-                        {body.filename}
-                      </option>
-                    ))}
-                  </select>
+                <select
+                  value={selectedBody}
+                  onChange={(e) => setSelectedBody(e.target.value)}
+                >
+                  <option value="">Select Body</option>
+                  {ImageData.images.bodies.map((body, index) => (
+                    <option key={index} value={index.toString()}>
+                      {body.filename}
+                    </option>
+                  ))}
+                </select>
 
-                  <select
-                    value={selectedAccessory}
-                    onChange={(e) => setSelectedAccessory(e.target.value)}
-                  >
-                    <option value="">Select Accessory</option>
-                    {ImageData.images.accessories.map((accessory, index) => (
-                      <option key={index} value={index.toString()}>
-                        {accessory.filename}
-                      </option>
-                    ))}
-                  </select>
+                <select
+                  value={selectedAccessory}
+                  onChange={(e) => setSelectedAccessory(e.target.value)}
+                >
+                  <option value="">Select Accessory</option>
+                  {ImageData.images.accessories.map((accessory, index) => (
+                    <option key={index} value={index.toString()}>
+                      {accessory.filename}
+                    </option>
+                  ))}
+                </select>
 
-                  <select
-                    value={selectedHead}
-                    onChange={(e) => setSelectedHead(e.target.value)}
-                  >
-                    <option value="">Select Head</option>
-                    {ImageData.images.heads.map((head, index) => (
-                      <option key={index} value={index.toString()}>
-                        {head.filename}
-                      </option>
-                    ))}
-                  </select>
+                <select
+                  value={selectedHead}
+                  onChange={(e) => setSelectedHead(e.target.value)}
+                >
+                  <option value="">Select Head</option>
+                  {ImageData.images.heads.map((head, index) => (
+                    <option key={index} value={index.toString()}>
+                      {head.filename}
+                    </option>
+                  ))}
+                </select>
 
-                  <select
-                    value={selectedGlasses}
-                    onChange={(e) => setSelectedGlasses(e.target.value)}
-                  >
-                    <option value="">Select Glasses</option>
-                    {ImageData.images.glasses.map((glasses, index) => (
-                      <option key={index} value={index.toString()}>
-                        {glasses.filename}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-              <div>
-                {isLoading ? (
-                  <div className="flex h-full items-center justify-center text-gray-700 dark:text-gray-300">
-                    Loading...
-                  </div>
-                ) : error ? (
-                  <div className="text-red-600 dark:text-red-400">
-                    Error: {error}
-                  </div>
-                ) : (
-                  <div className="grid grid-cols-1 gap-4 text-gray-900 md:grid-cols-2 lg:grid-cols-6 dark:text-gray-200">
-                    {seedsData.map(({ blockNumber, seed }) => (
-                      <div
-                        key={blockNumber}
-                        className="rounded border border-gray-200 bg-white p-4 text-center shadow-sm dark:border-gray-700 dark:bg-gray-800"
-                      >
-                        <SVGImage svgBase64={renderSVG(seed)} />
-                        <div className="mt-2 text-gray-700 dark:text-gray-300">
-                          Block Number: {blockNumber}
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                )}
+                <select
+                  value={selectedGlasses}
+                  onChange={(e) => setSelectedGlasses(e.target.value)}
+                >
+                  <option value="">Select Glasses</option>
+                  {ImageData.images.glasses.map((glasses, index) => (
+                    <option key={index} value={index.toString()}>
+                      {glasses.filename}
+                    </option>
+                  ))}
+                </select>
               </div>
             </div>
-          </section>
-        </div>
-      </QueryClientProvider>
-    </WagmiProvider>
+            <div>
+              {isLoading ? (
+                <div className="flex h-full items-center justify-center text-gray-700 dark:text-gray-300">
+                  Loading...
+                </div>
+              ) : error ? (
+                <div className="text-red-600 dark:text-red-400">
+                  Error: {error}
+                </div>
+              ) : (
+                <div className="grid grid-cols-1 gap-4 text-gray-900 md:grid-cols-2 lg:grid-cols-6 dark:text-gray-200">
+                  {seedsData.map(({ blockNumber, seed }) => (
+                    <div
+                      key={blockNumber}
+                      className="rounded border border-gray-200 bg-white p-4 text-center shadow-sm dark:border-gray-700 dark:bg-gray-800"
+                    >
+                      <SVGImage svgBase64={renderSVG(seed)} />
+                      <div className="mt-2 text-gray-700 dark:text-gray-300">
+                        Block Number: {blockNumber}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,33 @@ import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
 import type { AppProps } from 'next/app'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+const queryClient = new QueryClient()
+
+const config = createConfig({
+  chains: [mainnet, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   useLinguiInit(pageProps.translation)
   useLanguageAndDirection()
 
   return (
-    <>
-      <I18nProvider i18n={i18n}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </I18nProvider>
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <I18nProvider i18n={i18n}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </I18nProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
