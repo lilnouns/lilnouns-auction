67584ced084f013070a6253930050f44e1363439
Milad Nekofar <milad@nekofar.com>
2024-09-08 00:05:42 +0400

    feat(scheduler): add `seedHandler` to scheduled tasks
    integrate seeding logic to handle periodic database updates based on cron schedules. This ensures the database stays updated with the latest block data without overwhelming SQLite.

--- apps/scheduler/src/handlers/scheduled-handler.ts
+++ apps/scheduler/src/handlers/scheduled-handler.ts
@@ -1,6 +1,16 @@
 import { blockHandler } from '@/handlers/block-handler'
+import { seedHandler } from '@/handlers/seed-handler'
 import CronTime from 'cron-time-generator'
 
+/**
+ * Handles scheduled tasks based on predefined cron schedules.
+ * @param {ScheduledController} controller - The controller that holds the cron
+ *   schedule information.
+ * @param {Env} env - The environment settings and configurations necessary for
+ *   the handlers.
+ * @returns {Promise<void>} - A promise indicating the completion of the
+ *   scheduled task handling.
+ */
 export async function scheduledHandler(
   controller: ScheduledController,
   env: Env,
@@ -8,6 +18,7 @@ export async function scheduledHandler(
   switch (controller.cron) {
     case CronTime.everyMinute(): {
       await blockHandler(env)
+      await seedHandler(env)
       break
     }
     default: {
