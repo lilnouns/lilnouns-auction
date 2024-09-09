2e370dfec2f1f552b0ec3cab1523844624fb4c05
Milad Nekofar <milad@nekofar.com>
2024-09-09 18:46:13 +0400

    feat(frontend): add wallet connectors to `WagmiProvider`
    Enable various wallet connectors such as injected, MetaMask, Safe, and WalletConnect to improve wallet integration.

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -9,14 +9,18 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider, createConfig, http } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 
 const mainnetRpcUrl = process.env.NEXT_PUBLIC_MAINNET_RPC_URL
 const sepoliaRpcUrl = process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL
 
 const queryClient = new QueryClient()
 
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? ''
+
 const config = createConfig({
   chains: [mainnet, sepolia],
+  connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
   transports: {
     [mainnet.id]: http(mainnetRpcUrl),
     [sepolia.id]: http(sepoliaRpcUrl),
