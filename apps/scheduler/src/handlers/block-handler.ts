3b5206cf5f44ceb276a9df9a02e70fc545a38aeb
Milad Nekofar <milad@nekofar.com>
2024-09-09 10:59:43 +0400

    feat(scheduler): add block pagination and pool size limit
    Introduce pagination with `blockOffset` and limit fetch size with `poolSize` for better processing efficiency and error handling.

--- apps/scheduler/src/handlers/block-handler.ts
+++ apps/scheduler/src/handlers/block-handler.ts
@@ -10,28 +10,42 @@ import { pick } from 'remeda'
  */
 export async function blockHandler(env: Env): Promise<void> {
   try {
-    const blockOffset = 0
+    const poolSize = 100_000
+    let blockOffset = 0
+    let totalFetchedBlocks = 0
+    let moreBlocksAvailable = true
 
-    const blocks = await fetchBlocks(env, blockOffset).catch((error) => {
-      console.error('Error fetching blocks:', error)
-      throw error
-    })
-
-    const chunkSize = 50
-    const blocksData = blocks.map((block) =>
-      pick(block, ['id', 'number', 'timestamp']),
-    )
-
-    for (let i = 0; i < blocksData.length; i += chunkSize) {
-      const chunk = blocksData.slice(i, i + chunkSize)
-
-      try {
-        // Send each chunk as a separate message to the queue
-        await env.QUEUE.send({ type: 'blocks', data: { blocks: chunk } })
-      } catch (error) {
-        console.error('Error sending blocks to queue:', error)
+    while (moreBlocksAvailable) {
+      const blocks = await fetchBlocks(env, blockOffset).catch((error) => {
+        console.error('Error fetching blocks:', error)
         throw error
+      })
+
+      totalFetchedBlocks += blocks.length
+
+      if (blocks.length === 0 || totalFetchedBlocks >= poolSize) {
+        moreBlocksAvailable = false
+        break
       }
+
+      const chunkSize = 50
+      const blocksData = blocks.map((block) =>
+        pick(block, ['id', 'number', 'timestamp']),
+      )
+
+      for (let i = 0; i < blocksData.length; i += chunkSize) {
+        const chunk = blocksData.slice(i, i + chunkSize)
+
+        try {
+          // Send each chunk as a separate message to the queue
+          await env.QUEUE.send({ type: 'blocks', data: { blocks: chunk } })
+        } catch (error) {
+          console.error('Error sending blocks to queue:', error)
+          throw error
+        }
+      }
+
+      blockOffset += blocks.length
     }
   } catch (error) {
     console.error('Error processing Ethereum blocks:', error)
