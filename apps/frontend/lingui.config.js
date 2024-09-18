09c4cb13f481b47abec05f01c49d8ed1d306af93
Milad Nekofar <milad@nekofar.com>
2024-09-18 13:50:06 +0400

    feat(frontend): add 'pseudo' locale and fallback config
    Enable 'pseudo' locale for testing and set default fallback to 'en'.

--- apps/frontend/lingui.config.js
+++ apps/frontend/lingui.config.js
@@ -2,8 +2,12 @@ const { formatter } = require('@lingui/format-po')
 
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-  locales: ['ar', 'en', 'fa', 'tr'],
+  locales: ['ar', 'en', 'fa', 'tr', 'pseudo'],
+  pseudoLocale: 'pseudo',
   sourceLocale: 'en',
+  fallbackLocales: {
+    default: 'en',
+  },
   catalogs: [
     {
       path: '<rootDir>/src/locales/{locale}/messages',
