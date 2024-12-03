a06b957e2c3bfca737a19082208fa2d8a977bb59
Milad Nekofar <milad@nekofar.com>
2024-12-03 20:48:29 +0400

    feat(auction): integrate combobox component
    replace select elements with `AuctionCombobox` for better UX. This change improves the user interface by providing a more interactive and streamlined selection process.

--- apps/frontend/src/components/auction-combobox.tsx
+++ apps/frontend/src/components/auction-combobox.tsx
@@ -84,6 +84,7 @@ const AuctionCombobox: React.FC<AuctionComboboxProps> = ({
                 : selectedItem.filename),
           )
 
+    // Deselect if the same option is clicked
     if (index === selectedIndex) {
       setSelectedIndex(undefined)
       onChange()
@@ -117,6 +118,7 @@ const AuctionCombobox: React.FC<AuctionComboboxProps> = ({
         <ComboboxInput
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
+          onBlur={() => setQuery('')}
           displayValue={(item: AuctionItem | undefined) =>
             item && typeof item !== 'string' && item.filename === 'None'
               ? ''
