0ee3af4000a3cfd2027b327f2e46e3421bd28ba7
Milad Nekofar <milad@nekofar.com>
2025-01-19 13:15:25 +0300

    style(navbar): make navbar sticky with updated styles
    improve UX by keeping the navbar visible on scroll and aligning
    styling with design updates.

--- apps/frontend/src/components/navbar.tsx
+++ apps/frontend/src/components/navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   ];*/
 
   return (
-    <nav className="border-gray-200 bg-white dark:bg-gray-900">
-      <div className="mx-auto flex max-w-screen-xl items-center justify-between p-4">
+    <nav className="sticky top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
         <a href="#" className="flex items-center">
           <svg
             aria-label={t`Lil Nouns Auction`} // Using aria-label instead
