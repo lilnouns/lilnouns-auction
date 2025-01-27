a9ddb939b9382fca2361cac576d3027a42f099db
Milad Nekofar <milad@nekofar.com>
2025-01-27 23:00:46 +0300

    feat(frontend): enable button to open auction drawer
    restore button functionality to improve UI interaction and accessibility.

--- apps/frontend/src/components/auction-drawer.tsx
+++ apps/frontend/src/components/auction-drawer.tsx
@@ -2,7 +2,7 @@
 
 import { t } from '@lingui/macro'
 import { ImageData } from '@shared/utilities'
-import { Drawer } from 'flowbite-react'
+import { Button, Drawer } from 'flowbite-react'
 import React from 'react'
 import { HiBars2, HiSquaresPlus } from 'react-icons/hi2'
 import { join, map, pipe, split } from 'remeda'
@@ -37,9 +37,9 @@ const AuctionDrawer: React.FC<AuctionDrawerProps> = ({
 
   return (
     <>
-      {/*<div className="flex min-h-[50vh] items-center justify-center">
+      <div className="flex min-h-[50vh] items-center justify-center">
         <Button onClick={() => onOpen()}>Show swipeable drawer</Button>
-      </div>*/}
+      </div>
       <Drawer
         edge
         open={isOpen}
