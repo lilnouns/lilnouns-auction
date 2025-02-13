e01bbd6f5f679c5245d5ff117f3850afdef3cf12
Milad Nekofar <milad@nekofar.com>
2025-02-13 13:49:07 +0300

    refactor(frontend): extract `Providers` component
    simplify `_app` by moving provider logic to `Providers`. This improves readability, modularity, and maintainability.

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,86 +1,21 @@
+import '@repo/ui/globals.css'
+import type { AppProps } from 'next/app'
+
 import { Layout } from '@/components'
-import ErrorFallback from '@/components/error-fallback'
+import { Providers } from '@/components/providers'
+
 import { useLanguageAndDirection } from '@/hooks/use-language-and-direction'
 import { useLinguiInit } from '@/i18n/pages-router-i18n'
-import '@repo/ui/globals.css'
-import { i18n } from '@lingui/core'
-import { I18nProvider } from '@lingui/react'
-import type { AppProps } from 'next/app'
-import { ErrorBoundary } from 'react-error-boundary'
-import { find, mapToObj, pipe } from 'remeda'
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createConfig, http, WagmiProvider } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import {
-  coinbaseWallet,
-  injected,
-  metaMask,
-  safe,
-  walletConnect,
-} from 'wagmi/connectors'
-import { ThemeProvider } from '@/components/theme-provider'
-
-const activeChainId = Number(process.env.NEXT_PUBLIC_CHAIN_ID)
-const reownProjectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID ?? ''
-const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
-
-const activeChain =
-  pipe(
-    [mainnet, sepolia],
-    find((chain) => chain.id === activeChainId),
-  ) ?? sepolia
-
-const queryClient = new QueryClient()
-
-const chainNetworkMap: Record<number, string> = {
-  [mainnet.id]: 'eth-mainnet',
-  [sepolia.id]: 'eth-sepolia',
-}
-
-const transports = mapToObj([mainnet, sepolia], (chain) => [
-  chain.id,
-  http(
-    `https://${chainNetworkMap[chain.id]}.g.alchemy.com/v2/${alchemyApiKey}`,
-  ),
-])
-
-const config = createConfig({
-  chains: [activeChain],
-  connectors: [
-    injected(),
-    walletConnect({ projectId: reownProjectId }),
-    metaMask(),
-    coinbaseWallet({
-      appName: `Lil Nouns Auction`,
-    }),
-    safe(),
-  ],
-  transports,
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   useLinguiInit(pageProps.translation)
   useLanguageAndDirection()
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            <I18nProvider i18n={i18n}>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </I18nProvider>
-          </QueryClientProvider>
-        </WagmiProvider>
-      </ErrorBoundary>
-    </ThemeProvider>
+    <Providers>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Providers>
   )
 }
