f28016c00cac1147cb3b598a7049e386b2f5f4ee
Milad Nekofar <milad@nekofar.com>
2025-02-15 20:43:02 +0300

    feat(frontend): wrap `AuctionTraitFilter` in a drawer
    improve UX by adding a collapsible drawer for filtering options, enhancing clarity and saving space on the auction page.

--- apps/frontend/src/components/auction.tsx
+++ apps/frontend/src/components/auction.tsx
@@ -3,13 +3,48 @@
 import { AuctionTraitFilter } from '@/components/auction-trait-filter'
 import { AuctionPreviewGrid } from '@/components/auction-preview-grid'
 
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@repo/ui//components/drawer'
+import { Button } from '@repo/ui/components/button'
+
 export default function Auction() {
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-between p-1 py-5">
+      <div className="flex min-h-screen flex-col items-center justify-between p-1">
         <section className="w-full max-w-screen-xl p-1">
           <div className="container mx-auto">
-            <AuctionTraitFilter />
+            <Drawer>
+              <DrawerTrigger asChild>
+                <Button variant="outline">Open Drawer</Button>
+              </DrawerTrigger>
+              <DrawerContent>
+                <div className="mx-auto w-full">
+                  <DrawerHeader>
+                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                    <DrawerDescription></DrawerDescription>
+                  </DrawerHeader>
+                  <div className="p-4 pb-0">
+                    <AuctionTraitFilter />
+                  </div>
+                  <DrawerFooter>
+                    <DrawerClose>
+                      <Button className={'w-full'} variant="outline">
+                        Cancel
+                      </Button>
+                    </DrawerClose>
+                  </DrawerFooter>
+                </div>
+              </DrawerContent>
+            </Drawer>
+
             <AuctionPreviewGrid />
           </div>
         </section>
