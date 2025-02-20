f09ceae65f6e45135d410330c81408a25fe8eabb
Milad Nekofar <milad@nekofar.com>
2025-02-20 11:25:52 +0300

    refactor(frontend): restructure i18n and layout initialization
    simplify `RootLayout` and metadata generation for async handling. Improves clarity and consistency in i18n initialization.

--- apps/frontend/src/app/[lang]/layout.tsx
+++ apps/frontend/src/app/[lang]/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
 import linguiConfig from '@/../lingui.config'
 import { LinguiClientProvider } from '@/components/lingui-client-provider'
 import { allMessages, getI18nInstance } from '@/i18n/app-router-i18n'
-import { PageLangParam, withLinguiLayout } from '@/i18n/with-lingui'
+import { initLingui, PageLangParam } from '@/i18n/init-lingui'
 import { t } from '@lingui/core/macro'
+import { setI18n } from '@lingui/react/server'
 
 import ErrorFallback from '@/components/error-fallback'
 import { ErrorBoundary } from 'react-error-boundary'
@@ -23,42 +23,28 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 })
 
-export const metadata: Metadata = {
-  title: 'Lil Nouns Auction',
-  description: '',
-}
-
-/**
- * Generates an array of parameter objects for static site generation, where
- * each object contains a language code.
- *
- * @returns A promise that resolves to an array of objects, each with a 'lang'
- *   property.
- */
 export async function generateStaticParams() {
   return linguiConfig.locales.map((lang) => ({ lang }))
 }
 
-/**
- * Generates metadata for a page based on the provided language parameters.
- *
- * @param params.params
- * @param params - The parameters object.
- * @param params.lang - The language code to be used for translation.
- * @returns An object containing the translated title for the page.
- */
-export function generateMetadata({ params }: PageLangParam) {
-  const i18n = getI18nInstance(params.lang)
+export async function generateMetadata(props: PageLangParam) {
+  const i18n = getI18nInstance((await props.params).lang)
 
   return {
     title: t(i18n)`Translation Demo`,
   }
 }
 
-export default withLinguiLayout(function RootLayout({
+export default async function RootLayout({
   children,
-  params: { lang },
+  params,
+}: {
+  children: React.ReactNode
+  params: { lang: string }
 }) {
+  const lang = (await params).lang
+  initLingui(lang)
+
   return (
     <html lang={lang} className="dark">
       <body
@@ -83,4 +69,4 @@ export default withLinguiLayout(function RootLayout({
       </body>
     </html>
   )
-})
+}
