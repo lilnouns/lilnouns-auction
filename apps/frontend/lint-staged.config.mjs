da7b9a283ce2ad378d498211dd632f90594c0dfb
Milad Nekofar <milad@nekofar.com>
2025-02-25 15:54:56 +0300

    refactor(frontend): simplify and relocate lint-staged config
    improve config structure by targeting frontend-specific files. this reduces complexity and better isolates frontend linting.

---
+++ apps/frontend/lint-staged.config.mjs
@@ -0,0 +1,13 @@
+import path from "node:path";
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+/**
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
