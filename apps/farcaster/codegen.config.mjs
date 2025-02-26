b0e9d4133a4b697c6772d20bcdf2403f18d49aab
Milad Nekofar <milad@nekofar.com>
2025-02-26 15:03:14 +0300

    chore(farcaster): disable legacy CommonJS imports
    disable `emitLegacyCommonJSImports` to modernize module usage.
    This change helps ensure compatibility with modern ES module standards.

--- apps/farcaster/codegen.config.mjs
+++ apps/farcaster/codegen.config.mjs
@@ -2,6 +2,7 @@
  * @type {import('@graphql-codegen/cli').CodegenConfig}
  */
 export default {
+  emitLegacyCommonJSImports: false,
   generates: {
     './src/services/lilnouns/index.ts': {
       schema: process.env.LILNOUNS_SUBGRAPH_URL,
