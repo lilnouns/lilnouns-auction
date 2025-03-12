bf85784b1a027ec1fa9a812e394dd9fd96107edf
Milad Nekofar <milad@nekofar.com>
2025-03-12 13:42:32 +0300

    feat(frontend): add reusable `Icons` component
    introduce a new `Icons` component for SVG rendering. This improves maintainability and consistency across the application.

---
+++ apps/frontend/src/components/icons.tsx
@@ -0,0 +1,55 @@
+import type { HTMLAttributes } from 'react'
+
+type IconProps = HTMLAttributes<SVGElement>
+
+export const Icons = {
+  logo: (props: IconProps) => (
+    <svg
+      fill="none"
+      shapeRendering="crispEdges"
+      viewBox="0 0 20 8"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g fill="none">
+        <path fill="#F3322C" d="M11 0H3v1h8zm9 0h-8v1h8zM4 1H3v1h1z" />
+        <path fill="#FFF" d="M6 1H4v1h2z" />
+        <path fill="#000" d="M10 1H6v1h4z" />
+        <path fill="#F3322C" d="M11 1h-1v1h1zm2 0h-1v1h1z" />
+        <path fill="#FFF" d="M15 1h-2v1h2z" />
+        <path fill="#000" d="M19 1h-4v1h4z" />
+        <path fill="#F3322C" d="M20 1h-1v1h1zM4 2H3v1h1z" />
+        <path fill="#FFF" d="M6 2H4v1h2z" />
+        <path fill="#000" d="M10 2H6v1h4z" />
+        <path fill="#F3322C" d="M11 2h-1v1h1zm2 0h-1v1h1z" />
+        <path fill="#FFF" d="M15 2h-2v1h2z" />
+        <path fill="#000" d="M19 2h-4v1h4z" />
+        <path fill="#F3322C" d="M20 2h-1v1h1zM4 3H0v1h4z" />
+        <path fill="#FFF" d="M6 3H4v1h2z" />
+        <path fill="#000" d="M10 3H6v1h4z" />
+        <path fill="#F3322C" d="M13 3h-3v1h3z" />
+        <path fill="#FFF" d="M15 3h-2v1h2z" />
+        <path fill="#000" d="M19 3h-4v1h4z" />
+        <path fill="#F3322C" d="M20 3h-1v1h1zM1 4H0v1h1zm3 0H3v1h1z" />
+        <path fill="#FFF" d="M6 4H4v1h2z" />
+        <path fill="#000" d="M10 4H6v1h4z" />
+        <path fill="#F3322C" d="M11 4h-1v1h1zm2 0h-1v1h1z" />
+        <path fill="#FFF" d="M15 4h-2v1h2z" />
+        <path fill="#000" d="M19 4h-4v1h4z" />
+        <path fill="#F3322C" d="M20 4h-1v1h1zM1 5H0v1h1zm3 0H3v1h1z" />
+        <path fill="#FFF" d="M6 5H4v1h2z" />
+        <path fill="#000" d="M10 5H6v1h4z" />
+        <path fill="#F3322C" d="M11 5h-1v1h1zm2 0h-1v1h1z" />
+        <path fill="#FFF" d="M15 5h-2v1h2z" />
+        <path fill="#000" d="M19 5h-4v1h4z" />
+        <path fill="#F3322C" d="M20 5h-1v1h1zM4 6H3v1h1z" />
+        <path fill="#FFF" d="M6 6H4v1h2z" />
+        <path fill="#000" d="M10 6H6v1h4z" />
+        <path fill="#F3322C" d="M11 6h-1v1h1zm2 0h-1v1h1z" />
+        <path fill="#FFF" d="M15 6h-2v1h2z" />
+        <path fill="#000" d="M19 6h-4v1h4z" />
+        <path fill="#F3322C" d="M20 6h-1v1h1zm-9 1H3v1h8zm9 0h-8v1h8z" />
+      </g>
+    </svg>
+  ),
+}
