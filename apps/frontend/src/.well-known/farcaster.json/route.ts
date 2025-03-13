91938a3170b19c9663dbeff11684d9af0df6b1f9
Milad Nekofar <milad@nekofar.com>
2025-03-13 09:19:29 +0300

    feat(frontend): add `farcaster.json` route for config generation
    enable dynamic configuration for Farcaster integration.
    
    This change provides a `GET` endpoint to dynamically generate configuration required for Farcaster, enhancing integration capabilities.

---
+++ apps/frontend/src/.well-known/farcaster.json/route.ts
@@ -0,0 +1,22 @@
+export async function GET() {
+  const appUrl = process.env.NEXT_PUBLIC_SITE_URL
+  const accountAssociation =
+    process.env.NEXT_PUBLIC_FARCASTER_ACCOUNT_ASSOCIATION
+
+  const config = {
+    accountAssociation,
+    frame: {
+      version: '1',
+      name: 'Lil Nouns Auction',
+      iconUrl: `${appUrl}/icon.png`,
+      homeUrl: `${appUrl}`,
+      imageUrl: `${appUrl}/image.png`,
+      buttonTitle: 'Get Your Lil Noun',
+      splashImageUrl: `${appUrl}/splash.png`,
+      splashBackgroundColor: '#eeccff',
+      webhookUrl: `${appUrl}/api/webhook`,
+    },
+  }
+
+  return Response.json(config)
+}
