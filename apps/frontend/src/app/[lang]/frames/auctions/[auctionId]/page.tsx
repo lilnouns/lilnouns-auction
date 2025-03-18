d49a77565291df82a046a9902d9a86cf7013a5ef
Milad Nekofar <milad@nekofar.com>
2025-03-18 22:51:23 +0300

    feat(frontend): validate and display `auctionId`
    add validation for `auctionId` to handle invalid values.
    ensures better error handling and displays auction ID when valid.

--- apps/frontend/src/app/[lang]/frames/auctions/[auctionId]/page.tsx
+++ apps/frontend/src/app/[lang]/frames/auctions/[auctionId]/page.tsx
@@ -1,5 +1,11 @@
-export const runtime = 'edge';
+export const runtime = 'edge'
 
-export default function Page() {
-  return <div>Auction</div>
+export default function Page({ params }: { params: { auctionId: string } }) {
+  const auctionId = Number(params.auctionId)
+
+  if (isNaN(auctionId)) {
+    return <div>Invalid Auction ID</div>
+  }
+
+  return <div>Auction ID: {auctionId}</div>
 }
