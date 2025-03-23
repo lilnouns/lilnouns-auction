097a3363ae2f57329ff76099a4707d149878e163
Milad Nekofar <milad@nekofar.com>
2025-03-23 14:35:38 +0300

    refactor(frontend): rename `version` to `appVersion`
    enhance clarity and consistency by renaming `version` to `appVersion` in environment variable usages. This aligns variable names with their intended purpose and improves code readability.

--- apps/frontend/src/app/[lang]/frames/auctions/[auctionId]/page.tsx
+++ apps/frontend/src/app/[lang]/frames/auctions/[auctionId]/page.tsx
@@ -21,18 +21,18 @@ export async function generateMetadata(
   const i18n = getI18nInstance(lang)
 
   const appUrl = process.env.NEXT_PUBLIC_SITE_URL
-  const version = process.env.NEXT_PUBLIC_APP_VERSION
+  const appVersion = process.env.NEXT_PUBLIC_APP_VERSION
   const frameVersion = process.env.NEXT_PUBLIC_FRAME_VERSION ?? 1
 
   const frame: FrameEmbed = {
     version: 'next',
-    imageUrl: `${appUrl}/${lang}/frames/auctions/${auctionId}/opengraph-image?version=${version}`,
+    imageUrl: `${appUrl}/${lang}/frames/auctions/${auctionId}/opengraph-image?version=${appVersion}`,
     button: {
       action: {
         type: 'launch_frame',
         name: title ? title.absolute : t(i18n)`Lil Nouns Auction`,
-        url: `${appUrl}/${lang}/frames/?version=${version}`,
-        splashImageUrl: `${appUrl}/splash.png?version=${version}`,
+        url: `${appUrl}/${lang}/frames/?version=${appVersion}`,
+        splashImageUrl: `${appUrl}/splash.png?version=${appVersion}`,
         splashBackgroundColor: '#f7f7f7',
       },
       title: t(i18n)`Get Your Lil Noun`,
