ca6f862432d4bd0e08402f006086b8122f1a6cfe
Milad Nekofar <milad@nekofar.com>
2025-04-09 08:46:45 +0300

    feat(frontend): add dynamic Open Graph image generation
    introduce `Image` function for improved SEO and social sharing.

---
+++ apps/frontend/src/app/[lang]/opengraph-image.tsx
@@ -0,0 +1,58 @@
+import { redirect } from 'next/navigation'
+
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'Lil Nouns Auction'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+// Image generation
+export default async function Image() {
+  // const vazirmatnSemiBoldResp = await fetch(
+  //   new URL('../../styles/fonts/Vazirmatn/Vazirmatn-Bold.ttf', import.meta.url),
+  // )
+
+  // const vazirmatnSemiBoldFontArray = await vazirmatnSemiBoldResp.arrayBuffer()
+
+  // return new ImageResponse(
+  //   (
+  //     // ImageResponse JSX element
+  //     <div
+  //       style={{
+  //         color: '#131314',
+  //         fontSize: 128,
+  //         background: '#f0ecf9',
+  //         width: '100%',
+  //         height: '100%',
+  //         display: 'flex',
+  //         alignItems: 'center',
+  //         justifyContent: 'center',
+  //       }}
+  //     >
+  //       Lil Nouns Auction
+  //     </div>
+  //   ),
+  //   // ImageResponse options
+  //   {
+  //     // For convenience, we can re-use the exported opengraph-image
+  //     // size config to also set the ImageResponse's width and height.
+  //     ...size,
+  //     // fonts: [
+  //     //   {
+  //     //     name: 'Vazirmatn',
+  //     //     data: vazirmatnSemiBoldFontArray,
+  //     //     style: 'normal',
+  //     //     weight: 400,
+  //     //   },
+  //     // ],
+  //   },
+  // )
+
+  // Redirect to the static image in the public folder
+  redirect('/opengraph-image.png')
+}
